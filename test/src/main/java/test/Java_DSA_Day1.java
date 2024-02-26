b5dd2dc0fa3089cf1ef730281bcd69d8fb009751
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-26 14:26:49 +0800

    removeEvenIntFromArr exercise

--- test/src/main/java/test/Java_DSA_Day1.java
+++ test/src/main/java/test/Java_DSA_Day1.java
@@ -3,9 +3,32 @@ package test;
 public class Java_DSA_Day1 {
 	static void printArr(int[] arr) {
 		for(int i : arr) {
-			System.out.print(i);
+			System.out.print(i + " ");
 		}
 		System.out.println();
+	}
+	
+	static int[] removeEvenInt(int[] arr) {
+		int oddCount = 0;
+		
+		for(int i : arr) {
+			if(i % 2 != 0) {
+				oddCount++;
+			}
+		}
+		
+		int[] formattedArr = new int[oddCount];
+		
+		int index = 0;
+		
+		for(int i : arr) {
+			if(i % 2 != 0) {
+				formattedArr[index] = i;
+				index++;
+			}
+		}
+		
+		return formattedArr;
 	}
 	
 	public static void main(String[] args) {
@@ -15,8 +38,12 @@ public class Java_DSA_Day1 {
 		numArr[2] = 3;
 		numArr[3] = 4;
 		numArr[4] = 5;
-		
+		printArr(new int[] {3, 2, 1, 10, 9});
 		printArr(numArr);
+		
+		int[] evenNumRemArr = {3, 2, 1, 10, 9, 4, 6, 7, 5};
+		int[] result = removeEvenInt(evenNumRemArr);
+		printArr(result);
 	}
 
 }
