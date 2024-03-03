990a981940dacd081c67e57189d37f5e9af9fa40
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-03-03 22:01:14 +0800

    Print elements of a Singly Linked List algo

--- test/src/main/java/test/SinglyLinkedList.java
+++ test/src/main/java/test/SinglyLinkedList.java
@@ -1,17 +1,31 @@
 package test;
 
 public class SinglyLinkedList {
+	
 	private ListNode head;
 	
 	private static class ListNode {
-		private int data; // Generic type
-		private ListNode next;
-		
+		private int data; // Generic Type
+		private ListNode next; 
+		// Constructor class for Singly Linked List
 		public ListNode(int data) {
-			this.data = data;
-			this.next = null;
+			this.data = data; // data that this class will carry in each instance
+			this.next = null; // connected variable that will contain another instance of ListNode instance otherwise its null by default
 		}
 	}
+	
+	@SuppressWarnings("unused")
+	private static void PrintItemsInLinkedList(SinglyLinkedList sll){
+		// Printing elements of a Singly Linked List
+		ListNode current = sll.head;
+		
+		while(current != null) {
+			System.out.print(current.data + " ---> ");
+			current = current.next;
+		}	
+		System.out.print("null");
+	}
+	
 	public static void main(String[] args) {
 		SinglyLinkedList sll = new SinglyLinkedList();
 		sll.head = new ListNode(1);
@@ -23,6 +37,8 @@ public class SinglyLinkedList {
 		sll.head.next = second; // 1 ---> 2
 		second.next = third; // 1 ---> 2 ---> 3
 		third.next = fourth; // 1 ---> 2 ---> 3 ---> 4 ---> null
+		
+		PrintItemsInLinkedList(sll);
 	}
 
 }
